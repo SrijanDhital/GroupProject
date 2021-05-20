7a2e7389a048175e7ef251c9540e2a9f3298dc24
Bibek529 <60461830+Bibek529@users.noreply.github.com>
2021-05-20 17:13:16 +0545

    Add files via upload

---
+++ student.php
@@ -0,0 +1,48 @@
+<!--test for admin form-->
+<?php
+USE PHPUnit\Framework\TestCase;
+require 'functions/adminTest.php';
+
+class student extends TestCase{
+	function testAdminWithoutAll(){
+		$criteria = [
+			"name"=>"",
+			"email"=>"",
+			"password"=>""
+
+		];
+		$result=testAdm($criteria);
+		$this->assertFalse($result);
+	}
+	function testAdminWithoutEmail(){
+		$criteria = [
+			"name"=>"",
+			"email"=>"",
+			"password"=>"password"
+
+		];
+		$result=testAdm($criteria);
+		$this->assertFalse($result);
+	}
+	function testAdminWithoutPassword(){
+		$criteria = [
+			"name"=>"",
+			"email"=>"admin@admin.com",
+			"password"=>""
+
+		];
+		$result=testAdm($criteria);
+		$this->assertFalse($result);
+	}
+	function testAdminWithAll(){
+		$criteria = [
+			"name"=>"",
+			"email"=>"admin@admin.com",
+			"password"=>"password"
+
+		];
+		$result=testAdm($criteria);
+		$this->assertTrue($result);
+	}
+
+}
